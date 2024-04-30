77c4de3adf1dae5eff0322373050f0c3be3bb851
Eray Akgül <90953626+Erayakg@users.noreply.github.com>
2024-04-30 19:51:46 +0300

    docker commit 2

--- src/main/java/com/dockerDemo/demo/Controller.java
+++ src/main/java/com/dockerDemo/demo/Controller.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class Controller {
 
-    @GetMapping
-    public String deneme(){
-        return "deneme sezer";
-    }
+
     @GetMapping
     public String deneme2(){
         return "denemeasdas";
